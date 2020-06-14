5ede29593e69ec7c229a3ebd5cc0dffb0e6d9f16
Dhiraj Sable <dhiraj.sable@glassdoor.com>
2020-06-13 20:57:06 -0700

    Added Entity.

--- src/main/java/com/contact/login/logindetection/entity/DeviceMetadata.java
+++ src/main/java/com/contact/login/logindetection/entity/DeviceMetadata.java
@@ -2,4 +2,11 @@ package com.contact.login.logindetection.entity;
 
 public class DeviceMetadata {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Long userId;
+    private String deviceDetails;
+    private String location;
+    private Date lastLoggedIn;
 }
