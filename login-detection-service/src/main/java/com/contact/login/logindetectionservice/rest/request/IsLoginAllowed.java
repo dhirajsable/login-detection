7eee9b832adec2f7b93e239c7627aaca24eba143
Dhiraj Sable <sable.dhiraj@gmail.com>
2020-06-23 16:55:16 -0700

    Added modifications for
    1. New API to get details about the login device verification.

---
+++ login-detection-service/src/main/java/com/contact/login/logindetectionservice/rest/request/IsLoginAllowed.java
@@ -0,0 +1,14 @@
+package com.contact.login.logindetectionservice.rest.request;
+
+public class IsLoginAllowed {
+
+    long userId;
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+}
